69c6dbb6a244c4e4294c5b51d2322cad3984420b
Santosh Adap <Santosh.Adap@BRILLIO.COM>
2022-03-09 15:21:49 +0530

    Initial Comment

---
+++ src/proxy.conf.js
@@ -0,0 +1,17 @@
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/weatherforecast",
+     "/api/Data",
+      "/api/Workday",
+      "/api/Adobe",
+      "/api/SAPConcur",
+      "/api/Outlook",
+      "/api/Opentext"
+    ],
+    target: "https://localhost:7069",  //5001 
+    secure: false 
+  }
+]
+
+module.exports = PROXY_CONFIG;
